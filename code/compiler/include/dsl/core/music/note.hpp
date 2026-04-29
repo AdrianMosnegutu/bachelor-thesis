3538013d1ea334112c243938c1ee7f5b3383e96c
Moșneguțu Adrian-Ioan <adrianmosnegutuioan@yahoo.com>
2026-04-30 00:16:46 +0300

    refactor: separate note midi calculation into its own file

--- code/compiler/include/dsl/core/music/note.hpp
+++ code/compiler/include/dsl/core/music/note.hpp
@@ -10,13 +10,7 @@ struct Note {
     Accidental accidental;
     uint8_t octave;  // 0 - 8
 
-    [[nodiscard]] uint8_t midi_number() const {
-        static constexpr uint8_t NOTES_PER_OCTAVE = 12;
-        static constexpr uint8_t OCTAVE_OFFSET = 2;
-
-        return NOTES_PER_OCTAVE * (octave + OCTAVE_OFFSET) + static_cast<uint8_t>(pitch) +
-               static_cast<int8_t>(accidental);
-    }
+    [[nodiscard]] uint8_t midi_number() const;
 };
 
 }  // namespace dsl::music
